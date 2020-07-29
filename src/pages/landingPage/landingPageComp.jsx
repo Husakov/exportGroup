e2190f8b712265c7593fc7b4846b6474125a6beb
Samir Husak <samhusak@hotmail.com>
2020-07-29 20:43:55 +0200

    button style

--- src/pages/landingPage/landingPageComp.jsx
+++ src/pages/landingPage/landingPageComp.jsx
@@ -20,7 +20,7 @@ const LandingPageComp = () => {
                     <div className='label-3'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
-                    <input class="button-1" type="button" value="Posjeti webshop"/>
+                    <input className="btn1 mt-3" type="button" value="Posjeti webshop"/>
                 </Col>
                 <Col lg={4}></Col>
             </Row>
