68cab30b6e777bbb31a65c2cd5dfb56e640de2e5
Samir Husak <samhusak@hotmail.com>
2020-07-30 00:37:45 +0200

    route change

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import LandingPage from "./pages/landingPage";
 import SurvayPage from "./pages/surveyPage";
 const routes = {
   "/": () => <LandingPage />,
+  "/exportGroup": () => <LandingPage />,
   "/survey": () => <SurvayPage />
 };
 export default routes;
