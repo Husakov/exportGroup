944b1c5375bc28b7564bb71c2eb60f4f23151b7f
Samir Husak <samhusak@hotmail.com>
2020-07-30 00:45:10 +0200

    Updates

---
+++ precache-manifest.6fb5e544e695c03e3a342878329f6e0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caced2f3ee1406e806213055e89dda8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d5de96c49305d3f73f7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "75c2c385a7d7f816856a",
+    "url": "/static/css/main.c61cdbea.chunk.css"
+  },
+  {
+    "revision": "9d5de96c49305d3f73f7",
+    "url": "/static/js/2.20169585.chunk.js"
+  },
+  {
+    "revision": "a19c20645981503f58aa6bf9955c67ee",
+    "url": "/static/js/2.20169585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c2c385a7d7f816856a",
+    "url": "/static/js/main.3f1a618a.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  },
+  {
+    "revision": "5b3317fc609a7706e1cd85a48843cfb7",
+    "url": "/static/media/375_149_T13A0556@2x.5b3317fc.png"
+  },
+  {
+    "revision": "97e5c9d6c356763e1381a2d08f366dfe",
+    "url": "/static/media/MON686005@2x.97e5c9d6.png"
+  },
+  {
+    "revision": "96f8324a44e888cc8b47bf9ceead1038",
+    "url": "/static/media/delivery+2020+2@2x.96f8324a.png"
+  },
+  {
+    "revision": "23c2d7509840d13cc3a2de5ebb2c53b6",
+    "url": "/static/media/map@2x.23c2d750.png"
+  }
+]);
